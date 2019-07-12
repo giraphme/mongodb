6143948218ecb89fd9af4e07429f66ff2f9351d8
TamakiMaeda <giraph.me@gmail.com>
2019-07-12 17:40:15 +0900

    Support DocumentDB

--- lib/mongo/auth/scram.ex
+++ lib/mongo/auth/scram.ex
@@ -16,9 +16,9 @@ defmodule Mongo.Auth.SCRAM do
       with {:ok, %{"ok" => ok} = reply} when ok == 1 <- command(-2, message, s),
            {message, signature} = first(reply, first_bare, username, password, nonce),
            {:ok, %{"ok" => ok} = reply} when ok == 1 <- command(-3, message, s),
-           message = second(reply, signature),
+           message = second(reply, message, signature),
            {:ok, %{"ok" => ok} = reply} when ok == 1 <- command(-4, message, s),
-           do: final(reply)
+           do: final(reply, message)
 
     case result do
       :ok ->
@@ -34,7 +34,7 @@ defmodule Mongo.Auth.SCRAM do
   end
 
   defp first(
-         %{"conversationId" => 1, "payload" => server_payload, "done" => false},
+         %{"conversationId" => conversation_id, "payload" => server_payload, "done" => false},
          first_bare,
          username,
          password,
@@ -54,18 +54,33 @@ defmodule Mongo.Auth.SCRAM do
     server_signature = generate_signature(salted_password, auth_message)
     proof = generate_proof(salted_password, auth_message)
     client_final_message = %BSON.Binary{binary: "#{client_message},#{proof}"}
-    message = [saslContinue: 1, conversationId: 1, payload: client_final_message]
+    message = [saslContinue: 1, conversationId: conversation_id, payload: client_final_message]
 
     {message, server_signature}
   end
 
-  defp second(%{"conversationId" => 1, "payload" => payload, "done" => false}, signature) do
+  defp second(
+         %{"conversationId" => conversation_id, "payload" => payload, "done" => false},
+         [saslContinue: 1, conversationId: message_conversation_id, payload: _],
+         signature
+       )
+       when conversation_id == message_conversation_id do
     params = parse_payload(payload)
     ^signature = Base.decode64!(params["v"])
-    [saslContinue: 1, conversationId: 1, payload: %BSON.Binary{binary: ""}]
+    [saslContinue: 1, conversationId: conversation_id, payload: %BSON.Binary{binary: ""}]
   end
 
-  defp final(%{"conversationId" => 1, "payload" => %BSON.Binary{binary: ""}, "done" => true}) do
+  defp final(
+         %{
+           "conversationId" => conversation_id,
+           "payload" => %BSON.Binary{binary: ""},
+           "done" => true
+         },
+         saslContinue: 1,
+         conversationId: message_conversation_id,
+         payload: _
+       )
+       when conversation_id == message_conversation_id do
     :ok
   end
 
